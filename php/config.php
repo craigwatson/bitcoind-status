80065c340b37100c274fc217b88fd54fb8b8224c
Craig Watson <craig@cwatson.org>
2015-01-05 21:50:42 +0000

    Major rewrite

--- php/config.php
+++ php/config.php
@@ -1,16 +1,20 @@
 <?php
 
 $config = array(
-  'rpc_user'          => 'rpcuser',
-  'rpc_pass'          => 'rpcpass',
-  'rpc_host'          => 'localhost',
-  'rpc_port'          => '8332',
-  'rpc_ssl'           => FALSE,
-  'rpc_ssl_ca'        => NULL,
-  'cache_file'        => '/tmp/bitcoind-status-cache.php',
-  'max_cache_time'    => 300,
-  'nocache_whitelist' => array('127.0.0.1'),
-  'admin_email'       => 'admin@domain.com'
+  'rpc_user'               => 'rpcuser',
+  'rpc_pass'               => 'rpcpass',
+  'rpc_host'               => 'localhost',
+  'rpc_port'               => '8332',
+  'rpc_ssl'                => FALSE,
+  'rpc_ssl_ca'             => NULL,
+  'cache_file'             => '/tmp/bitcoind-status.cache',
+  'max_cache_time'         => 300,
+  'nocache_whitelist'      => array('127.0.0.1'),
+  'admin_email'            => 'admin@vikingserv.net',
+  'display_donation_text'  => FALSE,
+  'donation_address'       => 'not_set',
+  'donation_amount'        => '0.001',
+  'into_text'              => FALSE
 );
 
 ?>
--- php/functions.php
+++ php/functions.php
@@ -1,22 +1,27 @@
 <?php
 
+/** Connects to Bitcoin daemon and retrieves information, then writes to cache **/
 function get_raw_data() {
-
   global $config;
-  require_once('./php/easybitcoin.php');
 
+  // Include EasyBitcoin library and set up connection
+  require_once('./php/easybitcoin.php');
   $bitcoin = new Bitcoin($config['rpc_user'],$config['rpc_pass'],$config['rpc_host'],$config['rpc_port']);
 
+  // Setup SSL if configured
   if($config['rpc_ssl'] === TRUE) {
     $bitcoin->setSSL($config['rpc_ssl_ca']);
   }
-  
+
+  // Get info
   $data = $bitcoin->getinfo();
 
+  // Handle errors if they happened
   if (!$data) {
     $data['error'] = $bitcoin->error;
     $data['status'] = $bitcoin->status;
   } else {
+    // Set cache time and write cache
     $data['cache_time'] = time();
     write_to_cache($data);
   }
@@ -25,10 +30,18 @@ function get_raw_data() {
 
 }
 
+/** Simple function to serialize an array and write to file **/
 function write_to_cache($data) {
   global $config;
   $raw_data = serialize($data);
   file_put_contents($config['cache_file'],$raw_data);
+}
+
+/** Generates a QR Code image for donations **/
+function generate_donation_image() {
+  global $config;
+  $alt_text = 'Donate ' . $config['donation_amount'] . ' BTC to ' . $config['donation_address'];
+  return "\n" . '<img src="https://chart.googleapis.com/chart?chs=250x250&cht=qr&chl=' . $config['donation_address'] . '" alt="' . $alt_text . '" />' . "\n";
 }
 
 ?>
