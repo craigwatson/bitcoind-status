6b86138071323437b01b96550e97572b64f91d69
Craig Watson <craig@cwatson.org>
2015-08-16 12:58:58 +0100

    Reordering sample config

--- php/config.sample.php
+++ php/config.sample.php
@@ -10,36 +10,51 @@
  */
 
 $config = array(
-  'debug'                   => false,
+  // RPC
   'rpc_user'                => 'rpcuser',
   'rpc_pass'                => 'rpcpass',
   'rpc_host'                => 'localhost',
   'rpc_port'                => '8332',
   'rpc_ssl'                 => false,
   'rpc_ssl_ca'              => null,
-  'cache_file'              => '/tmp/bitcoind-status.cache',
-  'max_cache_time'          => 300,
-  'nocache_whitelist'       => array('127.0.0.1'),
-  'admin_email'             => 'admin@domain.net',
+
+  // Donations
   'display_donation_text'   => true,
   'donation_address'        => 'not_set',
   'donation_amount'         => '0.001',
-  'use_bitcoind_ip'         => true,
-  'intro_text'              => 'not_set',
+
+  // Peers
   'display_peer_info'       => false,
   'display_peer_port'       => false,
-  'geolocate_peer_ip'       => false,
   'hide_dark_peers'         => true,
+
+  // Cache
+  'use_cache'               => true,
+  'cache_file'              => '/tmp/bitcoind-status.cache',
+  'max_cache_time'          => 300,
+  'nocache_whitelist'       => array('127.0.0.1'),
+
+  // Geolocation
+  'geolocate_peer_ip'       => false,
+  'display_ip_location'     => false,
+
+  // UI
   'display_ip'              => false,
   'display_free_disk_space' => false,
-  'display_ip_location'     => false,
   'display_testnet'         => false,
   'display_version'         => true,
   'display_github_ribbon'   => true,
+  'display_max_height'      => true,
+  'use_bitcoind_ip'         => true,
+  'intro_text'              => 'not_set',
+
+  // Uptime
   'display_bitcoind_uptime' => false,
   'bitcoind_process_name'   => 'bitcoind',
-  'display_max_height'      => true,
-  'use_cache'               => true,
+
+  // System
   'date_format'             => 'H:i:s T, j F Y ',
-  'stylesheet'              => 'v2-light.css'
+  'stylesheet'              => 'v2-light.css',
+  'debug'                   => false,
+  'admin_email'             => 'admin@domain.net',
 );
