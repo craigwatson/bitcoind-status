7575e4cf7bf53647eb7831a45c21555210239fde
Craig Watson <craig@cwatson.org>
2016-02-14 23:54:36 +0000

    Fixing up geolocation

--- php/functions.php
+++ php/functions.php
@@ -275,9 +275,14 @@ function getGeolocation($ip_address)
 {
     global $config;
     global $country_codes;
-    $exec_result = json_decode(curlRequest("http://freegeoip.net/json/$ip_address"));
-    if (($exec_result !== false) & (in_array($exec_result['country_code'], $country_codes))) {
-        return trim($exec_result['country_code']);
+    $exec_result = curlRequest("http://freegeoip.net/json/$ip_address");
+    if ($exec_result !== false) {
+        $exec_array = json_decode($exec_result, true);
+        if (in_array($exec_array['country_code'], $country_codes)) {
+            return trim($exec_array['country_code']);
+        } else {
+            return 'blank';
+        }
     } else {
         return 'blank';
     }
