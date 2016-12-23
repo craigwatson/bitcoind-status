acd99f64db75521feebdb052546406a4223cf0f3
Craig Watson <craig@bashton.com>
2016-12-23 16:09:47 +0000

    Fixing scope on Puppet manifest

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,34 +1,34 @@
 node default {
 
-  include apt
+  include ::apt
 
   Class['::apt::update'] -> Package <| title != 'software-properties-common' |>
 
-  class { 'apache':
+  class { '::apache':
     mpm_module    => 'prefork',
     default_vhost => false,
   }
 
-  apache::vhost { $::fqdn:
+  ::apache::vhost { $::fqdn:
     port           => '80',
     docroot        => '/vagrant',
     manage_docroot => false,
   }
 
-  include php::params
-  include php::apache
-  include php::extension::curl
-  include apache::mod::php
+  include ::php::params
+  include ::php::apache
+  include ::php::extension::curl
+  include ::apache::mod::php
 
   Php::Extension <| |> -> Php::Config <| |> ~> Service['httpd']
 
-  class { 'bitcoind':
+  class { '::bitcoind':
     rpcallowip          => ['127.0.0.1'],
     rpcpassword         => 'statustest',
     rpcuser             => 'status',
     testnet             => true,
     disablewallet       => true,
-    use_bitcoin_classic => true,
+    use_bitcoin_classic => false,
   }
 
 }
