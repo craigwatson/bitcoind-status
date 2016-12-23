d18887df5ac944abb1ebec9920e5a5a7bb998f9b
Craig Watson <craig@bashton.com>
2016-12-23 16:14:04 +0000

    Adding Vagrant config

---
+++ php/config.vagrant.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Bitcoin Status Page
+ *
+ * @category File
+ * @package  BitcoinStatus
+ * @author   Craig Watson <craig@cwatson.org>
+ * @license  https://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
+ * @link     https://github.com/craigwatson/bitcoind-status
+ */
+
+$config = array(
+  // RPC
+  'rpc_user'                  => 'status',
+  'rpc_pass'                  => 'statustest',
+  'rpc_host'                  => 'localhost',
+  'rpc_port'                  => '8332',
+  'rpc_ssl'                   => false,
+  'rpc_ssl_ca'                => null,
+
+  // Donations
+  'display_donation_text'     => true,
+  'donation_address'          => 'not_set',
+  'donation_amount'           => '0.001',
+
+  // Peers
+  'display_peer_info'         => true,
+  'display_peer_port'         => false,
+  'hide_dark_peers'           => true,
+  'ignore_unknown_ping'       => true,
+  'peers_to_ignore'           => array(),
+
+  // Cache
+  'use_cache'                 => true,
+  'cache_file'                => '/var/tmp/bitcoind-status.cache',
+  'max_cache_time'            => 300,
+  'nocache_whitelist'         => array('127.0.0.1'),
+
+  // Geolocation
+  'geolocate_peer_ip'         => false,
+  'display_ip_location'       => false,
+
+  // UI
+  'display_ip'                => true,
+  'display_free_disk_space'   => false,
+  'display_testnet'           => false,
+  'display_version'           => true,
+  'display_github_ribbon'     => false,
+  'display_max_height'        => true,
+  'use_bitcoind_ip'           => true,
+  'intro_text'                => 'not_set',
+  'display_bitnodes_info'     => false,
+  'display_chart'             => true,
+  'display_peer_chart'        => true,
+  'node_links'                => array(),
+
+  // Stats
+  'stats_whitelist'           => array('127.0.0.1'),
+  'stats_file'                => '/var/tmp/bitcoind-status.data',
+  'stats_max_age'             => '604800',
+  'stats_min_data_points'     => 5,
+
+  // Node Count
+  'peercount_whitelist'       => array('127.0.0.1'),
+  'peercount_file'            => '/var/tmp/bitcoind-peers.data',
+  'peercount_max_age'         => '2592000',
+  'peercount_min_data_points' => 5,
+
+  // Uptime
+  'display_bitcoind_uptime'   => false,
+  'bitcoind_process_name'     => 'bitcoind',
+
+  // System
+  'date_format'               => 'H:i:s T, j F Y ',
+  'timezone'                  => null,
+  'stylesheet'                => 'v2-light.css',
+  'debug'                     => false,
+  'admin_email'               => 'craig@cwatson.org',
+);
